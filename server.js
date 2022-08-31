d0edc0c2357a5a96af791a26569610c0a91c14c8
Christa <christanewing47@gmail.com>
2022-08-31 13:55:48 -0400

    complete server

--- server.js
+++ server.js
@@ -12,14 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // M O N G O  C O N N E C T I O N
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:/", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost:27017/social-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 mongoose.set("debug", true);
 
 app.use(require("./routes"));
 
 // I N I T I A L I Z E  S E V E R  L I S T E N E R
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
