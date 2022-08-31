c752de36750b55e891b185f8a672f1ab971b8952
Christa <christanewing47@gmail.com>
2022-08-31 10:42:57 -0400

    require other dependencies

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// D E P E N D E N C I E S
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 // I N I T I A L I Z E  S E V E R  L I S T E N E R
 app.listen(5050, () => {
   console.log("Sever running!");
 });
+// com
