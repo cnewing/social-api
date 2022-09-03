f45a003a47d3aa3ae97128327475d3c26d6e72c6
Christa <christanewing47@gmail.com>
2022-09-03 14:59:58 -0400

    start user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,6 +3,21 @@ const { User } = require("../models");
 // U S E R  A C T I O N S
 
 // G E T  A L L  U S E R S
+const userController = {
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: "posts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
 // G E T  A  U S E R  B Y  I D
 
@@ -15,3 +30,5 @@ const { User } = require("../models");
 // A D D  A  F R I E N D
 
 // D E L E T E  A  F R I E N D
+
+module.exports = userController;
