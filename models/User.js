0d4f3817fe824a469678c1d2cde36f229f39446b
Christa <christanewing47@gmail.com>
2022-09-03 14:47:14 -0400

    found missing ) & finished user.js

--- models/User.js
+++ models/User.js
@@ -1,42 +1,47 @@
 const { Schema, model } = require("mongoose");
 
 // U S E R  S C H E M A
-const userSchema = new Schema({
-
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  email:{
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    match: [/.+@+\..+/]
-  },
-  posts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Post'
-        }
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [/.+@+\..+/],
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
     ],
     friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
-},
-{
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
     toJSON: {
-        virtuals: true
+      virtuals: true,
     },
-    id: false
-};
+    id: false,
+  }
+);
 
 // F R I E N D  C O U N T
-userSchema.virtual('friendCount').get(function () {
-    return this.friends.length;
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
+
+const User = model("User", userSchema);
+
+module.exports = User;
