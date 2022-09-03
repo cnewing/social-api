a5733acb44c9a8d5f2e956d88d70a6e67f8a3bbe
Christa <christanewing47@gmail.com>
2022-09-03 14:20:52 -0400

    setup schema structuring

---
+++ models/Post.js
@@ -0,0 +1,6 @@
+const { Schema, model, Types } = require("mongoose");
+const moment = require("moment");
+
+// P O S T  S C H E M A
+
+// R E A C T I O N  S C H E M A
---
+++ models/User.js
@@ -0,0 +1,5 @@
+const { Schema, model } = require("mongoose");
+
+// U S E R  S C H E M A
+
+// F R I E N D  C O U N T
