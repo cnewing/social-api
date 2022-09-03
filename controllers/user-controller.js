6eaabc0ca02a3f25cf39cb4cd5ee796ad6d9dc8c
Christa <christanewing47@gmail.com>
2022-09-03 14:05:28 -0400

    setup user controller structure

---
+++ controllers/user-controller.js
@@ -0,0 +1,17 @@
+const { User } = require("../models");
+
+// U S E R  A C T I O N S
+
+// G E T  A L L  U S E R S
+
+// G E T  A  U S E R  B Y  I D
+
+// C R E A T E  A  U S E R
+
+// D E L E T E  A  U S E R
+
+// U P D A T E  A  U S E R
+
+// A D D  A  F R I E N D
+
+// D E L E T E  A  F R I E N D
