8144ae0e4dcc77ab08cbfba62ef8dc76983b323d
Christa <christanewing47@gmail.com>
2022-09-03 16:17:13 -0400

    complete post js while troubleshooting

--- models/Post.js
+++ models/Post.js
@@ -2,5 +2,69 @@ const { Schema, model, Types } = require("mongoose");
 const moment = require("moment");
 
 // P O S T  S C H E M A
+const postSchema = new Schema(
+  {
+    postText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dataFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // R E A C T I O N  S C H E M A
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dataFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+// G E T  T H E  N U M B E R  O F  R E A C T I O N S
+postSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+const Post = model("Post", postSchema);
+
+module.exports = Post;
