c61595c455902a75a45b7661fb8c8f3497e6ca2f
Christa <christanewing47@gmail.com>
2022-09-03 12:49:31 -0400

    update

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -12,8 +12,20 @@ const postController = {
       .then((dbPostData) => res.json(dbPostData))
       .catch((err) => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
   // G E T  A  P O S T  B Y  I D
-  
+  getPostById({ params }, res) {
+    Post.findOne({ _id: params.id })
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbPostData => res.json(dbPostData)
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
