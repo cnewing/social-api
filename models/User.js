a474a80fb465a872906502820efbf887aaa24bb3
Christa <christanewing47@gmail.com>
2022-09-03 14:34:53 -0400

    update user.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,33 @@
 const { Schema, model } = require("mongoose");
 
 // U S E R  S C H E M A
-
+const userSchema = new Schema({
+    const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email:{
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: [/.+@+\..+/]
+  },
+  posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+});
 // F R I E N D  C O U N T
